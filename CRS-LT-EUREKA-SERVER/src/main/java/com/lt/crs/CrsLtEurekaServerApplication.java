9ee2fbf66adfe4c83208d4fa3ee5bc0de6a5e3d7
sohambhowmick <sohamslc@gmail.com>
2022-03-04 11:58:40 +0530

    adding project structure

---
+++ CRS-LT-EUREKA-SERVER/src/main/java/com/lt/crs/CrsLtEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.lt.crs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrsLtEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrsLtEurekaServerApplication.class, args);
+	}
+
+}
