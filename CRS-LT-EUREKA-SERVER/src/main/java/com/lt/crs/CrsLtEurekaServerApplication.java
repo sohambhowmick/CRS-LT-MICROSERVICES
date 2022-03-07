9ca2de2dacccc2b2729456ec814fb04af5a83e2d
sohambhowmick <sohamslc@gmail.com>
2022-03-07 11:34:09 +0530

    added comments in eureka server

--- CRS-LT-EUREKA-SERVER/src/main/java/com/lt/crs/CrsLtEurekaServerApplication.java
+++ CRS-LT-EUREKA-SERVER/src/main/java/com/lt/crs/CrsLtEurekaServerApplication.java
@@ -4,8 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+//Eureka server for all the 4 microservices/eureka clients
 @SpringBootApplication
-@EnableEurekaServer
+@EnableEurekaServer //Declares this Spring Boot App as the eureka server
 public class CrsLtEurekaServerApplication {
 
 	public static void main(String[] args) {
